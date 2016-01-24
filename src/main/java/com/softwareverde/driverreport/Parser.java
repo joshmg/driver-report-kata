a8f28baabe8d9ae50e94e699c4c41ee266cf3841
Josh Green <joshmg@gmail.com>
2016-01-24 13:36:59 -0500

    Implemented parser.

---
+++ src/main/java/com/softwareverde/driverreport/Parser.java
@@ -0,0 +1,63 @@
+package com.softwareverde.driverreport;
+
+public class Parser {
+    final String _spaceDeliminator = " ";
+    final String _newlineDeliminator = "\n";
+    String _content = "";
+    Integer _position = 0;
+
+    // NOTE: Returns EOF if not found...
+    private Integer _getNextSpaceIndex() {
+        final Integer index = _content.indexOf(_spaceDeliminator, _position);
+
+        if (index < 0) {
+            return _content.length();
+        }
+
+        return index;
+    }
+
+    // NOTE: Returns EOF if not found...
+    private Integer _getNextNewlineIndex() {
+        final Integer index = _content.indexOf(_newlineDeliminator, _position);
+
+        if (index < 0) {
+            return _content.length();
+        }
+
+        return index;
+    }
+
+    private Integer _getNextDelimiterIndex() {
+        final Integer spaceIndex = _getNextSpaceIndex();
+        final Integer newlineIndex = _getNextNewlineIndex();
+
+        return (spaceIndex < newlineIndex ? spaceIndex : newlineIndex);
+    }
+
+    public void setContent(String content) {
+        _content = content;
+        _position = 0;
+    }
+
+    public String getNextWord() {
+        if (_position >= _content.length()) {
+            return "";
+        }
+
+        if (! _content.contains(_spaceDeliminator)) {
+            String nextWord = _content.substring(_position);
+            _position += nextWord.length();
+            return nextWord;
+        }
+
+        Integer endIndex = _getNextDelimiterIndex();
+        if (endIndex < 0) {
+            endIndex = _content.length() - 1;
+        }
+
+        String nextWord = _content.substring(_position, endIndex);
+        _position += nextWord.length() +1;
+        return nextWord;
+    }
+}
---
+++ src/test/java/com/softwareverde/driverreport/ParserTest.java
@@ -0,0 +1,127 @@
+package com.softwareverde.driverreport;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParserTest {
+    Parser _subject;
+
+    @Before
+    public void setUp() throws Exception {
+        _subject = new Parser();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testParserRetrievesFirstWord() throws Exception {
+        // Setup
+        final String content = "Mary Had A Little Lamb\nHer fleece was white as snow\n";
+        _subject.setContent(content);
+        final String expectedFirstWord = "Mary";
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the first word.", expectedFirstWord, word);
+    }
+
+    @Test
+    public void testParserRetrievesSecondWord() throws Exception {
+        // Setup
+        final String content = "Mary Had A Little Lamb\nHer fleece was white as snow\n";
+        _subject.setContent(content);
+        final String expectedSecondWord = "Had";
+        _subject.getNextWord();
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the second word.", expectedSecondWord, word);
+    }
+
+    @Test
+    public void testParserReturnsEmptyStringWhenPastEndOfContent() throws Exception {
+        // Setup
+        final String content = "Mary Had";
+        _subject.setContent(content);
+        final String expectedThirdWord = "";
+        _subject.getNextWord();
+        _subject.getNextWord();
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Should return empty string when EOF.", expectedThirdWord, word);
+    }
+
+    @Test
+    public void testParserReturnsWholeStringWhenDeliminatorNotPresent() throws Exception {
+        // Setup
+        final String content = "MaryHadALittleLamb";
+        _subject.setContent(content);
+        final String expectedFirstWord = "MaryHadALittleLamb";
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the first word when content lacks a deliminator.", expectedFirstWord, word);
+    }
+
+    @Test
+    public void testParserReturnsEmptyStringWhenRetrievingTheNextWordWithContentHavingNoDeliminator() throws Exception {
+        // Setup
+        final String content = "MaryHadALittleLamb";
+        _subject.setContent(content);
+        final String expectedFirstWord = "MaryHadALittleLamb";
+        final String expectedSecondWord = "";
+        final String firstWord = _subject.getNextWord();
+
+        // Action
+        final String secondWord = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the second word when content is at EOF.", expectedSecondWord, secondWord);
+    }
+
+    @Test
+    public void testParserReturnsNextWordWhenDelimitedWithNewline() throws Exception {
+        // Setup
+        final String content = "Mary Had\nA Little Lamb";
+        _subject.setContent(content);
+        final String expectedSecondWord = "Had";
+        _subject.getNextWord();
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the next word when it is delimited by a new line.", expectedSecondWord, word);
+    }
+
+    @Test
+    public void testParserReturnsNextWordWhenAtEndOfLine() throws Exception {
+        // Setup
+        final String content = "Mary Had\nA Little Lamb";
+        _subject.setContent(content);
+        final String expectedThirdWord = "A";
+        _subject.getNextWord();
+        _subject.getNextWord();
+
+        // Action
+        final String word = _subject.getNextWord();
+
+        // Assert
+        assertEquals("Failed to retrieve the next word when it falls on a new line.", expectedThirdWord, word);
+    }
+}
